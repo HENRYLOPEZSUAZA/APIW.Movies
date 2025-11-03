e55ef3ba549b7a1e0add3eecc4086847169260a6
HENRYLOPEZSUAZA <henrylopezitm@gmail.com>
2025-11-03 16:46:41 -0500

    Add project files.

---
+++ APIW.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using APIW.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIW.Movies.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ APIW.Movies/DAL/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIW.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] //este data anotation indica que el campo es obligatorio
+        [Display(Name = "Nombre de la Catgegoria")] //este data anotation indica el nombre que se mostrara en las vistas]
+        public string Name { get; set; }
+
+
+
+    }
+}
