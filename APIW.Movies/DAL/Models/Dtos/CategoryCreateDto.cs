e2f0d522a57ef4817eb55e323c55c3cf42d32bf0
HENRYLOPEZSUAZA <henrylopezitm@gmail.com>
2025-11-04 00:15:27 -0500

    Automapper and Dtos created

---
+++ APIW.Movies/DAL/Models/Dtos/CategoryCreateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIW.Movies.DAL.Models.Dtos
+{
+    public class CategoryCreateDto
+    {   [Required(ErrorMessage ="El nombre de la categoria es obligatoria.")]
+        [MaxLength(100, ErrorMessage ="El nombre de la categoria no debe exceder los 100 caracteres.")]
+        public string Name { get; set; }
+    }
+}
