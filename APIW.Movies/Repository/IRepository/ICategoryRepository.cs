bdf69e0c6bbd7a2ac0cebfb7c585bf7609d58b1c
HENRYLOPEZSUAZA <henrylopezitm@gmail.com>
2025-11-03 23:58:38 -0500

    New categoryRepository Created

---
+++ APIW.Movies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using APIW.Movies.DAL.Models;
+
+namespace APIW.Movies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync();// me retorna una lista de categorias
+        Task<Category> GetCategoryAsync(int id);// me retorna una categoria por su id
+        Task<bool> CategoryExistsByIdAsync(int id);// me retorna true o false si existe una categoria por su id
+        Task<bool> CategoryExistsByNameAsync(string name);// me retorna true o false si existe una categoria por su nombre
+        Task<bool> CreateCategoryAsync(Category category);// me crea una categoria
+        Task<bool> UpdateCategoryAsync(Category category);// me actualiza una categoria
+        Task<bool> DeleteCategoryAsync(int id);// me elimina una categoria
+
+    }
+}
