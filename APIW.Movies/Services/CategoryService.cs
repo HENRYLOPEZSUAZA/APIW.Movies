75a20b23245b5272d69a633f8b027acfd58061be
HENRYLOPEZSUAZA <henrylopezitm@gmail.com>
2025-11-11 20:43:52 -0500

    Paso 1 faltan 25 min dle video

---
+++ APIW.Movies/Services/CategoryService.cs
@@ -0,0 +1,56 @@
+﻿using APIW.Movies.DAL.Models;
+using APIW.Movies.DAL.Models.Dtos;
+using APIW.Movies.Repository.IRepository;
+using APIW.Movies.Services.IServices;
+using AutoMapper;
+
+namespace APIW.Movies.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<CategoryDto>> GetCategoriesAsync()
+        {
+            var categories =  _categoryRepository.GetCategoriesAsync();//llamo el metodo del repositorio
+            return _mapper.Map<ICollection<CategoryDto>>(categories);//mapeo los datos obtenidos del repositorio a DTOs
+            
+        }
+
+        public Task<CategoryDto> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
